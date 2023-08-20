33d1295786fef9c379ce10746e3b07248f6ad147
Teo <teocns@gmail.com>
2023-08-20 14:05:56 -0600

    PatchAccessor & PatchValue updated

--- content/renderer/fingerprinting/utilities.cc
+++ content/renderer/fingerprinting/utilities.cc
@@ -19,6 +19,7 @@
 
 
 
+
 namespace fingerprinting{
 
 
@@ -27,26 +28,68 @@ namespace fingerprinting{
 
 
 inline void patchWrapper(const v8::FunctionCallbackInfo<v8::Value>& innerArgs) {
+    v8::Isolate* isolate = innerArgs.GetIsolate();
     v8::Local<v8::Context> context = innerArgs.GetIsolate()->GetCurrentContext();
     v8::Local<v8::Object> callbackData = v8::Local<v8::Object>::Cast(innerArgs.Data());
-    v8::Local<v8::Function> handler = v8::Local<v8::Function>::Cast(
-        callbackData->Get(context, v8::String::NewFromUtf8(innerArgs.GetIsolate(), "handler").ToLocalChecked()).ToLocalChecked()
-    );
-    int argc = innerArgs.Length(); // Get the number of arguments
+    
 
-    // Create an array to hold the arguments
-    v8::Local<v8::Value>* argv = new v8::Local<v8::Value>[argc];
+    v8::Local<v8::Value> valueOverride = callbackData->Get(context, v8::String::NewFromUtf8(isolate, "handler").ToLocalChecked()).ToLocalChecked();
 
-    // Populate the array with argument values
-    for (int i = 0; i < argc; ++i) {
+
+    if (!valueOverride->IsFunction()) {
+        innerArgs.GetReturnValue().Set(valueOverride);
+        return;
+    }
+
+    // Target will be passed for getter/setter functions. We want to pass it as the first argument
+    v8::MaybeLocal<v8::Value> maybeTarget = callbackData->Get(context, v8::String::NewFromUtf8(isolate, "target").ToLocalChecked());
+
+    const bool hasTarget = !maybeTarget.IsEmpty();
+    const int argc = innerArgs.Length() + (hasTarget ? 1 : 0);
+
+    v8::Local<v8::Value> argv[argc];
+
+
+    int argvStartIdx = 0;
+    if (hasTarget) {
+        v8::Local<v8::Value> target = maybeTarget.ToLocalChecked();
+        argv[0] = target;
+        argvStartIdx = 1;
+    }
+    
+
+    for (int i = argvStartIdx; i < argc; ++i) {
         argv[i] = innerArgs[i];
     }
 
-    v8::Local<v8::Value> result = handler->Call(context, innerArgs.Holder(), argc, argv).ToLocalChecked();
-    innerArgs.GetReturnValue().Set(result);
+    
 
-    // Don't forget to delete the allocated array when done
-    delete[] argv;
+    
+
+
+    v8::Local<v8::Function> handler = v8::Local<v8::Function>::Cast(valueOverride);
+
+
+
+    // argv[0] = target;
+    // // Get the "thisArg" object so that it is passed as second parameter.
+    // // Similar as you would get when calling Function.prototype.apply(thisArg, [arguments])
+
+    // argv[1] = innerArgs.This();
+
+    // Populate the array with argument values
+    
+
+    v8::MaybeLocal<v8::Value> maybeResult = handler->Call(context, innerArgs.Holder(), argc, argv);
+    
+    if (!maybeResult.IsEmpty()) {
+        // If the result is empty, an exception has occurred,
+        // so we need to handle the exception
+        innerArgs.GetReturnValue().Set(maybeResult.ToLocalChecked());    
+    }
+    else{
+        innerArgs.GetReturnValue().SetUndefined();
+    }
 }
 
 
@@ -94,7 +137,7 @@ static void PatchAccessor(const v8::FunctionCallbackInfo<v8::Value>& args) {
         return args[index];
     };
 
-    v8::Local<v8::Object> target = v8::Local<v8::Object>::Cast(ValidateArg(0, true, "Expected first argument to be an object (target)."));
+    v8::Local<v8::Object> holder = v8::Local<v8::Object>::Cast(ValidateArg(0, true, "Expected first argument to be an object (holder)."));
     v8::Local<v8::String> propName = v8::Local<v8::String>::Cast(ValidateArg(1, false, "Expected second argument to be a string (property name)."));
     v8::Local<v8::Object> config = v8::Local<v8::Object>::Cast(ValidateArg(2, true, "Expected third argument to be an object (config)."));
 
@@ -112,7 +155,7 @@ static void PatchAccessor(const v8::FunctionCallbackInfo<v8::Value>& args) {
         return;
     }
 
-    v8::MaybeLocal<v8::Value> maybeDescriptor = target->GetOwnPropertyDescriptor(context, propName);
+    v8::MaybeLocal<v8::Value> maybeDescriptor = holder->GetOwnPropertyDescriptor(context, propName);
     
     if (maybeDescriptor.IsEmpty()) {
         ThrowAndLogError(isolate, "Property does not exist on target object.");
@@ -142,12 +185,14 @@ static void PatchAccessor(const v8::FunctionCallbackInfo<v8::Value>& args) {
             // }
             
             (void)callbackData->Set(context, v8::String::NewFromUtf8(isolate, "handler").ToLocalChecked(), v8::Local<v8::Function>::Cast(config->Get(context, accessorType).ToLocalChecked()));
-
-
+            (void)callbackData->Set(context, v8::String::NewFromUtf8(isolate, "target").ToLocalChecked(), nativeFn);
+            
             v8::Local<v8::FunctionTemplate> templateFn = v8::FunctionTemplate::New(isolate,
                 patchWrapper,
                 callbackData
             );
+
+            templateFn->SetClassName(propName);
 
             return templateFn->GetFunction(context).ToLocalChecked();
         }
@@ -164,11 +209,11 @@ static void PatchAccessor(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
 
 
-    v8::PropertyAttribute propertyAttributes = target->GetPropertyAttributes(context, propName).FromJust();
+    v8::PropertyAttribute propertyAttributes = holder->GetPropertyAttributes(context, propName).FromJust();
 
     propertyAttributes = (v8::PropertyAttribute)(propertyAttributes | v8::PropertyAttribute::Patched);
 
-    target->SetAccessorProperty(propName, patchedGetterFn, patchedSetterFn, propertyAttributes, v8::DEFAULT);
+    holder->SetAccessorProperty(propName, patchedGetterFn, patchedSetterFn, propertyAttributes, v8::DEFAULT);
 }
 
 
@@ -200,18 +245,18 @@ static void PatchValue(const v8::FunctionCallbackInfo<v8::Value>& args) {
         return args[index];
     };
 
-    v8::Local<v8::Object> target = v8::Local<v8::Object>::Cast(ValidateArg(0, true, "Expected first argument to be an object (target)."));
+    v8::Local<v8::Object> holder = v8::Local<v8::Object>::Cast(ValidateArg(0, true, "Expected first argument to be an object (holder)."));
     v8::Local<v8::String> propName = v8::Local<v8::String>::Cast(ValidateArg(1, false, "Expected second argument to be a string (property name)."));
-    v8::Local<v8::Value> valueOverride = args[2];
+    v8::Local<v8::Value> handler = args[2];
 
-    v8::MaybeLocal<v8::Value> maybeDescriptor = target->GetOwnPropertyDescriptor(context, propName);
+    v8::MaybeLocal<v8::Value> maybeDescriptor = holder->GetOwnPropertyDescriptor(context, propName);
     
     if (maybeDescriptor.IsEmpty()) {
         ThrowAndLogError(isolate, "Property does not exist on target object.");
         return;
     }
 
-    // v8::Local<v8::Value> currentValue = maybeDescriptor.ToLocalChecked();
+    
 
     v8::Local<v8::Object> descriptor = v8::Local<v8::Object>::Cast(maybeDescriptor.ToLocalChecked());
 
@@ -223,31 +268,30 @@ static void PatchValue(const v8::FunctionCallbackInfo<v8::Value>& args) {
     }
 
 
-    // Get original value 
-
-    // v8::Local<v8::Value> originalValue = descriptor->Get(context, __VALUE__).ToLocalChecked();
-
+    // The target object being patched (the property of the holder)
+    v8::Local<v8::Value> target = descriptor->Get(context, __VALUE__).ToLocalChecked();
 
 
-    v8::Local<v8::Value> finalValue = valueOverride;
+    
 
-    if (valueOverride->IsFunction()) {
-        // We need to double-wrap the function to ensure that it is masked (native function)
 
-        v8::Local<v8::Object> callbackData = v8::Object::New(isolate);
-        (void)callbackData->Set(context, v8::String::NewFromUtf8(isolate, "handler").ToLocalChecked(), valueOverride);
-        v8::Local<v8::FunctionTemplate> templateFn = v8::FunctionTemplate::New(isolate,
-            patchWrapper,
-            callbackData
-        );
-        finalValue = v8::Local<v8::Value>::Cast(templateFn->GetFunction(context).ToLocalChecked());
+    if (!handler->IsFunction()) {
+        ThrowAndLogError(isolate, "Expected third argument to be a function.");
+        return;
     }
 
 
-    // (void)callbackData->Set(context, v8::String::NewFromUtf8(isolate, "originalValue").ToLocalChecked(), originalValue);
 
-    v8::PropertyAttribute propertyAttributes = target->GetPropertyAttributes(context, propName).FromJust();
 
+    v8::Local<v8::Object> wrapperInnerArgs = v8::Object::New(isolate);
+
+    
+    (void)wrapperInnerArgs->Set(context, v8::String::NewFromUtf8(isolate, "handler").ToLocalChecked(), handler).ToChecked();
+    (void)wrapperInnerArgs->Set(context, v8::String::NewFromUtf8(isolate, "holder").ToLocalChecked(), holder).ToChecked();
+    (void)wrapperInnerArgs->Set(context, v8::String::NewFromUtf8(isolate, "target").ToLocalChecked(), target).ToChecked();
+
+
+    v8::PropertyAttribute propertyAttributes = holder->GetPropertyAttributes(context, propName).FromJust();
 
     // Assign v8::PropertyAttribute::Patched to propertyAttributes
 
@@ -256,12 +300,12 @@ static void PatchValue(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
     
     // Re-apply with the original property attributes
-    v8::Maybe<bool> maybesucceess = target->SetAccessor(
+    v8::Maybe<bool> maybesucceess = holder->SetAccessor(
         context,
         propName,
         AccessorGetterWrapper,
         AccessorSetterWrapper, 
-        finalValue,
+        wrapperInnerArgs,
         v8::DEFAULT,
         propertyAttributes,
         v8::SideEffectType::kHasSideEffect,
@@ -281,22 +325,84 @@ static void PatchValue(const v8::FunctionCallbackInfo<v8::Value>& args) {
 private:
 
 
-    static void AccessorGetterWrapper(v8::Local<v8::Name> name, const v8::PropertyCallbackInfo<v8::Value>& info) {
+    static void AccessorGetterWrapper(v8::Local<v8::Name> name, const v8::PropertyCallbackInfo<v8::Value>& innerArgs) {
+        /**
+         * 
+         * The actual function() that executes when the value is accessed
+         * i.e 
+         * PatchValue(obj, "prop", function() { ... });
+         * The last argument is this function
+         * We need to invoke the handler with the following arguments:
+         * 1. The original value (target)
+         * 2. 
+        */
+        v8::Isolate* isolate = innerArgs.GetIsolate();
+        v8::HandleScope handle_scope(isolate);
+        v8::Local<v8::Context> context = isolate->GetCurrentContext();
+        v8::Local<v8::Object> callbackData = v8::Local<v8::Object>::Cast(innerArgs.Data());
 
-        // Retrieve the callback data
-
-
-        v8::Local<v8::Value> originalData = info.Data();
         
-        // Verify if it's a function
+        // The user-defined handler. Might return a primitive, or a function.
+        v8::Local<v8::Function> handler = v8::Local<v8::Function>::Cast(
+            callbackData->Get(context, v8::String::NewFromUtf8(isolate, "handler").ToLocalChecked()).ToLocalChecked()
+        );
 
-        // if (!originalData->IsFunction()) {
-        //     v8::Local<v8::Function> func = v8::Local<v8::Function>::Cast(originalData);
-        //     v8::Local<v8::Value> result = func->Call(info.GetIsolate()->GetCurrentContext(), info.This(), 0, nullptr).ToLocalChecked();
-        //     return info.GetReturnValue().Set(result);    
-        // }
 
-        info.GetReturnValue().Set(originalData);
+        // The target patched
+        // In javascript, this is commonly referred to as "target"
+        v8::Local<v8::Value> target = callbackData->Get(context, v8::String::NewFromUtf8(isolate, "target").ToLocalChecked()).ToLocalChecked();
+
+        const int argc=3;
+
+        v8::Local<v8::Value> argv[argc] = {
+            target,
+            innerArgs.This(),
+            name
+        };
+
+        v8::MaybeLocal<v8::Value> maybeResult = handler->Call(context, innerArgs.Holder(), argc, argv);
+
+        if (maybeResult.IsEmpty()) {
+            innerArgs.GetReturnValue().SetUndefined();
+            return;
+        }
+
+
+        // If the result is a function, we need to wrap it again to ensure that it is masked (native function)
+
+
+        v8::Local<v8::Value> result = maybeResult.ToLocalChecked();
+
+
+        // If the result is a function, we need to wrap it again to ensure that it is masked (native function)
+
+        if (result->IsFunction()) {
+
+            // Create new callback data
+            v8::Local<v8::Object> innerFnCallbackData = v8::Object::New(isolate);
+            (void)innerFnCallbackData->Set(context, v8::String::NewFromUtf8(isolate, "handler").ToLocalChecked(), result).ToChecked();
+            // (void)innerFnCallbackData->Set(context, v8::String::NewFromUtf8(isolate, "target").ToLocalChecked(), target).ToChecked();
+
+            v8::Local<v8::FunctionTemplate> templateFn = v8::FunctionTemplate::New(
+                isolate,
+                patchWrapper,
+                innerFnCallbackData
+            );
+
+            // set classname from the `v8::Local<v8::Name> name` parameter. Convert it to local<stirng>
+            v8::Local<v8::String> funcName = v8::Local<v8::String>::Cast(
+                name
+            );
+
+            templateFn->SetClassName(funcName);
+
+            innerArgs.GetReturnValue().Set(templateFn->GetFunction(context).ToLocalChecked());
+            return;
+        }
+
+        innerArgs.GetReturnValue().Set(result);
+
+
     }
 
     static void AccessorSetterWrapper(v8::Local<v8::Name> name, v8::Local<v8::Value> value, const v8::PropertyCallbackInfo<void>& info) {
