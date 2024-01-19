01d255fd3b9d7861929d6d2eec5f224a88618120
Teo <teocns@gmail.com>
2024-01-18 21:27:38 -0600

    Re-add //fingerprinting

---
+++ fingerprinting/public/cpp/manager.h
@@ -0,0 +1,54 @@
+// fingerprint_manager.h
+#ifndef FINGERPRINTING_MANAGER_IMPL_H_
+#define FINGERPRINTING_MANAGER_IMPL_H_
+
+#include <string>
+
+#include "base/files/file_util.h"
+#include "base/json/json_reader.h"
+#include "base/logging.h"
+#include "base/no_destructor.h"
+#include "base/strings/string_util.h"
+
+#include "base/values.h"
+#include "fingerprinting/core/device_descriptor/fingerprint_impl.h"
+#include "fingerprinting/core/manager.h"
+#include "fingerprinting/public/cpp/export.h"
+#include "fingerprinting/public/cpp/mixins/evasions.mojom.h"
+#include "fingerprinting/public/cpp/mixins/user-agent.mojom.h"
+#include "fingerprinting/public/mojom/manager.mojom.h"
+
+#include "mojo/public/cpp/bindings/receiver.h"  // Add this line
+#include "mojo/public/cpp/bindings/receiver_set.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+#include "fingerprinting/public/cpp/common.h"
+namespace fingerprinting {
+
+class FINGERPRINTING_PUBLIC_EXPORT FingerprintManager
+    : virtual public internal::FingerprintManagerBase,
+      public internal::UAMixinMojom,
+      public internal::EvasionsMixinMojom {
+ public:
+  static FingerprintManager* GetInstance(bool try_init = false);
+
+  void Bind(mojo::PendingReceiver<fingerprinting::mojom::FingerprintManager>
+                receiver);
+
+  void Enabled(EnabledCallback callback) override;
+  void GetFingerprintString(GetFingerprintStringCallback) override;
+
+ private:
+  FingerprintManager();
+
+  ~FingerprintManager() override;
+  friend class base::NoDestructor<FingerprintManager>;
+
+  mojo::ReceiverSet<fingerprinting::mojom::FingerprintManager> receivers_;
+};
+
+FINGERPRINTING_PUBLIC_EXPORT FingerprintManager* manager(bool try_init = false);
+
+}  // namespace fingerprinting
+
+#endif  // FINGERPRINTING_MANAGER_IMPL_H_
---
+++ fingerprinting/utility/user_agent.cc
@@ -0,0 +1,173 @@
+// Copyright 2015 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fingerprinting/utility/user_agent.h"
+#include <map>
+#include <regex>
+#include <string>
+#include "base/logging.h"
+#include "base/pickle.h"
+#include "base/values.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+#include "third_party/blink/public/common/user_agent/user_agent_metadata.h"
+
+namespace fingerprinting::utility::user_agent {
+namespace {
+
+/**
+* UACH Hints:
+
+
+architecture
+bitness
+model
+platformVersion
+uaFullVersion
+fullVersionList
+
+"highEntropyValues": {
+    "brands": [
+      {
+        "brand": "Chromium",
+        "version": "115"
+      },
+      {
+        "brand": "Not)A;Brand",
+        "version": "24"
+      },
+      {
+        "brand": "Google Chrome",
+        "version": "115"
+      }
+    ],
+    "mobile": false,
+    "bitness": "64",
+    "platform": "macOS",
+    "architecture": "arm",
+
+    "platformVersion": "13.3.1"
+  }
+
+*
+*/
+
+
+constexpr char kMobileFormFactor[] =
+    "Mobile";  // Must reflect the same value as in
+               // components/embedder_support/user_agent_utils.h
+
+constexpr char kFullVersion[] = "uaFullVersion";  // Might be depreacted
+constexpr char kPlatform[] = "platform";
+constexpr char kPlatformVersion[] = "platformVersion";
+constexpr char kArchitecture[] = "architecture";
+constexpr char kModel[] = "model";
+constexpr char kMobile[] = "mobile";
+constexpr char kBitness[] = "bitness";
+// constexpr char kWow64[] = "wow64";
+constexpr char kBrands[] = "brands";
+constexpr char kFullVersionList[] = "fullVersionList";
+
+constexpr char kBrand[] = "brand";
+constexpr char kVersion[] = "version";
+
+bool populate_brands_list(blink::UserAgentBrandList& out,
+                          base::Value::List* brands_list) {
+  /**
+   * Iterates over raw brands list and populates the UserAgentBrandList
+   * Returns false if the operation failed
+   */
+  if (!brands_list || !(brands_list->size() > 0)) {
+    return false;
+  }
+  for (size_t i = 0; i < brands_list->size(); ++i) {
+    const base::Value* brand_version = &(*brands_list)[i];
+
+    const base::Value::Dict* bv = brand_version->GetIfDict();
+    if (!bv) {
+      return false;
+    }
+
+    const std::string* brand = bv->FindString(kBrand);
+    const std::string* version = bv->FindString(kVersion);
+
+    if (!(brand && version)) {
+      return false;
+    }
+    out.push_back(blink::UserAgentBrandVersion(*brand, *version));
+  }
+
+  return true;
+}
+
+}  // Anonymous namespace
+
+bool MakeUserAgentMetadata(base::Value::Dict& in,
+                           blink::UserAgentMetadata& out) {
+  if (!populate_brands_list(out.brand_version_list, in.FindList(kBrands))) {
+    LOG(ERROR) << "Failed parsing UA Metadata: brand_version_list invalid.";
+    return false;
+  }
+
+  if (!populate_brands_list(out.brand_full_version_list,
+                            in.FindList(kFullVersionList))) {
+    LOG(ERROR)
+        << "Failed parsing UA Metadata: brand_full_version_list invalid.";
+    return false;
+  }
+
+  const std::string* full_version = in.FindString(kFullVersion);
+  const std::string* platform = in.FindString(kPlatform);
+  const std::string* platform_version = in.FindString(kPlatformVersion);
+  const std::string* architecture = in.FindString(kArchitecture);
+  const std::string* model = in.FindString(kModel);
+  absl::optional<bool> mobile = in.FindBool(kMobile);
+  bool is_mobile = mobile.value_or(false);
+  const std::string* bitness = in.FindString(kBitness);
+  // absl::optional<bool> wow64 = highEntropyValues.FindBool(kWow64)
+
+  // Deprecated. Don't fail if not found
+  if (full_version != nullptr) {
+    out.full_version = *full_version;
+  }
+
+  if (platform == nullptr) {
+    LOG(ERROR) << "Failed parsing UA Metadata: platform field not set.";
+    return false;
+  }
+  out.platform = *platform;
+
+  if (platform_version == nullptr) {
+    LOG(ERROR) << "Failed parsing UA Metadata: platform_version field not set.";
+    return false;
+  }
+  out.platform_version = *platform_version;
+
+  if (architecture == nullptr) {
+    LOG(ERROR) << "Failed parsing UA Metadata: architecture field not set.";
+    return false;
+  }
+  out.architecture = *architecture;
+
+  if (model != nullptr) {
+    out.model = *model;
+  }
+
+  out.mobile = is_mobile;
+
+  if (bitness == nullptr) {
+    LOG(ERROR) << "Failed parsing UA Metadata: bitness field not set.";
+    return false;
+  }
+  out.bitness = *bitness;
+
+  // wow64
+  // out.WriteBool(wow64.value_or(false));;
+  out.wow64 = false;
+
+  out.form_factor = is_mobile ? kMobileFormFactor : "";
+
+  return true;
+}
+
+}  // namespace fingerprinting
