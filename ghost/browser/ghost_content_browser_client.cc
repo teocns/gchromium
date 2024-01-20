0bdaaa120815a076ca3372cf93daa42eb0222c15
Teo <teocns@gmail.com>
2024-01-20 00:24:23 -0600

    wip: add //fingerprinting to ContentBrowserClient

--- ghost/browser/ghost_content_browser_client.cc
+++ ghost/browser/ghost_content_browser_client.cc
@@ -1,53 +1,30 @@
+#include "ghost/browser/ghost_content_browser_client.h"
 #include <algorithm>
 #include <optional>
 #include <string>
 #include <utility>
 #include <vector>
-//
 #include "base/command_line.h"
 #include "base/logging.h"
-// #include "base/functional/bind.h"
-// #include "base/json/json_reader.h"
-// #include "base/strings/strcat.h"
-// #include "base/system/sys_info.h"
-// #include "build/build_config.h"
-// #include "chrome/browser/browser_process.h"
-// #include "chrome/browser/chrome_browser_interface_binders.h"
-// #include "chrome/browser/chrome_content_browser_client.h"
-// #include
-// "chrome/browser/content_settings/host_content_settings_map_factory.h"
-// #include
-// "components/content_settings/core/browser/host_content_settings_map.h"
-// #include "components/embedder_support/switches.h"
-// #include "components/prefs/pref_service.h"
-// #include "components/version_info/version_info.h"
-// #include "content/public/browser/browser_context.h"
-// #include "content/public/browser/browser_task_traits.h"
-// #include "content/public/browser/browser_thread.h"
-// #include "content/public/browser/browser_url_handler.h"
-// #include "content/public/browser/navigation_handle.h"
-// #include "content/public/browser/render_frame_host.h"
-// #include "content/public/browser/render_process_host.h"
-// #include "content/public/browser/storage_partition.h"
-// #include "content/public/browser/weak_document_ptr.h"
-// #include "content/public/browser/web_ui_browser_interface_broker_registry.h"
-// #include "content/public/browser/web_ui_controller_interface_binder.h"
-// #include "content/public/common/content_client.h"
-// #include "content/public/common/content_switches.h"
-// #include "content/public/common/url_constants.h"
-// #include "extensions/buildflags/buildflags.h"
-#include "ghost/browser/ghost_content_browser_client.h"
-// #include "mojo/public/cpp/bindings/pending_receiver.h"
-// #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "chrome/browser/chrome_content_browser_client.h"
+
+#include "content/browser/renderer_host/render_process_host_impl.h"
+#include "content/public/browser/render_process_host.h"
+#include "fingerprinting/public/cpp/manager.h"
+#include "fingerprinting/public/mojom/manager.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
+
 // #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 // #include "net/cookies/site_for_cookies.h"
 // #include "services/service_manager/public/cpp/binder_registry.h"
 // #include
-// "third_party/blink/public/common/associated_interfaces/associated_interface_registry.h"
-// #include "third_party/blink/public/common/features.h"
+
+#include "third_party/blink/public/common/features.h"
+
 // #include "third_party/blink/public/common/loader/url_loader_throttle.h"
-// #include
-// "third_party/blink/public/mojom/webpreferences/web_preferences.mojom.h"
 // #include "third_party/widevine/cdm/buildflags.h"
 // #include "ui/base/l10n/l10n_util.h"
 
@@ -57,6 +34,183 @@
 // using content::RenderFrameHost;
 // using content::WebContents;
 
-GhostContentBrowserClient::GhostContentBrowserClient() {
+using content::BrowserThread;
+using content::ContentBrowserClient;
+using content::RenderFrameHost;
+using content::WebContents;
+using fingerprinting::FingerprintManager;
+GhostContentBrowserClient::GhostContentBrowserClient()
+    : ChromeContentBrowserClient() {
   LOG(INFO) << "GhostContentBrowserClient::GhostContentBrowserClient";
 }
+
+void GhostContentBrowserClient::ExposeInterfacesToRenderer(
+    service_manager::BinderRegistry* registry,
+    blink::AssociatedInterfaceRegistry* associated_registry,
+    content::RenderProcessHost* render_process_host) {
+  ChromeContentBrowserClient::ExposeInterfacesToRenderer(
+      registry, associated_registry, render_process_host);
+
+  // We can downcast the RenderProcessHost to RenderProcessHostImpl safe to do
+  // so. it here since we're in the browser render_process_host
+
+  content::RenderProcessHostImpl* render_process_host_impl =
+      static_cast<content::RenderProcessHostImpl*>(render_process_host);
+
+  render_process_host_impl->AddUIThreadInterface(
+      registry,
+      base::BindRepeating(
+          [](mojo::PendingReceiver<fingerprinting::mojom::FingerprintManager>
+                 receiver) {
+            fingerprinting::FingerprintManager::GetInstance()->Bind(
+                std::move(receiver));
+          }));
+}
+
+// @@ -2653,6 +2654,13 @@ void RenderProcessHostImpl::RegisterMojoInterfaces() {
+//                            instance_weak_factory_.GetWeakPtr()));
+//  #endif
+//
+//   // Register Fingerprinting interfaces
+//
+//   AddUIThreadInterface(
+//       registry.get(),
+//       base::BindRepeating(&fingerprinting::FingerprintManager::Bind,
+//       base::Unretained(fingerprinting::FingerprintManager::GetInstance()))
+//   );
+// ---- Please do not register interfaces below this line ------
+//
+// This call should be done after registering all interfaces above, so that
+// registry->AddReceiver<FingerprintManager>(
+//     base::BindRepeating(
+//         &FingerprintManager::Bind,
+//         base::Unretained(FingerprintManager::GetInstance())),
+//     content::GetUIThreadTaskRunner({}));
+//
+//
+//
+//
+//
+// auto taskRunner = content::GetUIThreadTaskRunner({});
+// auto ref = &fingerprinting::FingerprintManager::Bind;
+// AddUIThreadInterface(
+//     registry,
+//   );
+//
+
+// // Helper method that checks the RenderProcessHost is still alive before
+// hopping
+// // over to the IO thread.
+// void MaybeBindFingerprinting(
+//     int rph_id,
+//     content::ResourceContext* resource_context,
+//     base::RepeatingCallback<scoped_refptr<safe_browsing::UrlCheckerDelegate>()>
+//         get_checker_delegate,
+//     mojo::PendingReceiver<safe_browsing::mojom::SafeBrowsing> receiver) {
+//   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+//
+//   content::RenderProcessHost* render_process_host =
+//       content::RenderProcessHost::FromID(rph_id);
+//   if (!render_process_host)
+//     return;
+//
+//   content::GetIOThreadTaskRunner({})->PostTask(
+//       FROM_HERE,
+//       base::BindOnce(&safe_browsing::MojoSafeBrowsingImpl::MaybeCreate,
+//       rph_id,
+//                      resource_context, std::move(get_checker_delegate),
+//                      std::move(receiver)));
+// }
+
+// Register Fingerprinting interfaces
+
+// registry->AddInterface<fingerprinting::mojom::FingerprintManager>(
+//       base::BindRepeating(
+//           &media::MediaCapsImpl::AddReceiver,
+//           base::Unretained(cast_browser_main_parts_->media_caps())),
+//       base::SingleThreadTaskRunner::GetCurrentDefault());
+//
+
+// GetTaskRunnerForThread()
+// registry->AddInterface<mojom::MemoryPressureController>(
+//     base::BindRepeating(&MemoryPressureControllerImpl::AddReceiver,
+//                         base::Unretained(memory_pressure_controller_.get())),
+//     base::SingleThreadTaskRunner::GetCurrentDefault());
+
+// registry->AddInterface(
+//     base::BindRepeating(&Initialize, render_process_host->GetID()),
+//     content::GetUIThreadTaskRunner({}));
+
+// content::BrowserContext* context =
+// render_process_host->GetBrowserContext(); mojo::MakeSelfOwnedReceiver(
+//     std::make_unique<fingerprinting::mojom::BraveSearchFallbackHost>(
+//         context->GetDefaultStoragePartition()
+//             ->GetURLLoaderFactoryForBrowserProcess()),
+//     std::move(receiver));
+// }
+// void GhostContentBrowserClient::ExposeInterfacesToRenderer(
+//     service_manager::BinderRegistry* registry,
+//     blink::AssociatedInterfaceRegistry* associated_registry,
+//     content::RenderProcessHost* render_process_host) {
+//   ChromeContentBrowserClient::ExposeInterfacesToRenderer(
+//       registry, associated_registry, render_process_host);
+
+// registry->AddUIThreadInterface(
+//     base::BindRepeating(&Initialize),
+//     base::SequencedTaskRunnerHandle::Get()
+// );
+
+// registry->AddInterface(
+//     base::BindRepeating(&Initialize, render_process_host->GetID()),
+//     content::GetUIThreadTaskRunner({}));
+// }
+// void GhostContentBrowserClient::ExposeInterfacesToRenderer(
+//     service_manager::BinderRegistry* registry,
+//     blink::AssociatedInterfaceRegistry* associated_registry,
+//     content::RenderProcessHost* render_process_host) {
+//   ChromeContentBrowserClient::ExposeInterfacesToRenderer(
+//       registry, associated_registry, render_process_host);
+//
+//   // registry->AddInterface(base::BindRepeating(&BindFingerprintingManager,
+//   // render_process_host->GetID()),
+//   //                        content::GetUIThreadTaskRunner({}));
+// }
+
+//
+//
+//   AddUIThreadInterface(
+//       registry.get(),
+//       base::BindRepeating(&fingerprinting::FingerprintManager::Bind,
+//       base::Unretained(fingerprinting::FingerprintManager::GetInstance()))
+//   );
+//
+//
+//
+
+// void BindFingerprintingManager(
+//     mojo::PendingReceiver<fingerprinting::mojom::FingerprintManager>
+//     receiver) {
+//   fingerprint_manager_.AddReceiver(std::move(receiver));
+// }
+
+// void BindBraveSearchDefaultHost(
+//     content::RenderFrameHost* const frame_host,
+//     mojo::PendingReceiver<brave_search::mojom::BraveSearchDefault> receiver)
+//     {
+//   auto* context = frame_host->GetBrowserContext();
+//   auto* profile = Profile::FromBrowserContext(context);
+//   if (brave::IsRegularProfile(profile)) {
+//     auto* template_url_service =
+//         TemplateURLServiceFactory::GetForProfile(profile);
+//     const std::string host = frame_host->GetLastCommittedURL().host();
+//     mojo::MakeSelfOwnedReceiver(
+//         std::make_unique<brave_search::BraveSearchDefaultHost>(
+//             host, template_url_service, profile->GetPrefs()),
+//         std::move(receiver));
+//   } else {
+//     // Dummy API which always returns false for private contexts.
+//     mojo::MakeSelfOwnedReceiver(
+//         std::make_unique<brave_search::BraveSearchDefaultHostPrivate>(),
+//         std::move(receiver));
+//   }
+// }
